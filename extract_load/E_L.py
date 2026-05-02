bbcec7ce45f68620ccbcb56a928b31fe956f1ef0
phauzer <phauzer05@gmail.com>
2026-05-02 16:19:47 +0200

    feat: add assets metadata api call and table writing into GBQ

--- extract_load/E_L.py
+++ extract_load/E_L.py
@@ -73,6 +73,24 @@ def gbq_get_tickers(conf: dict) -> list:
     return tickers
 
 
+def gbq_get_all_tickers(conf: dict) -> list:
+
+    sql = f"""
+    SELECT
+    Ticker
+    FROM {conf["transactions_table"]}
+    GROUP BY 1
+    """
+
+    df_tickers = pandas_gbq.read_gbq(
+        sql, project_id=conf["project"], credentials=conf["credentials"]
+    )
+
+    tickers = df_tickers["Ticker"].to_list()
+
+    return tickers
+
+
 def gbq_get_currencies(conf: dict) -> list:
 
     sql = f"""
@@ -100,6 +118,32 @@ def gbq_get_currencies(conf: dict) -> list:
     currencies = df_currencies["Currency code"].to_list()
 
     return currencies
+
+
+def api_get_asset_metadata(asset_tickers: list, conf: dict) -> pd.DataFrame:
+
+    metadata_list = []
+    for ticker in asset_tickers:
+
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": conf["tiingo_api_token"],
+        }
+
+        response = requests.get(
+            f"https://api.tiingo.com/tiingo/daily/{ticker}", headers=headers
+        )
+
+        r = response.json()
+        metadata = {
+            "tickers": ticker,
+            "asset_name": r["name"],
+            "exchange_code": r["exchangeCode"],
+        }
+        metadata_list.append(metadata)
+
+    df_metadata = pd.DataFrame(metadata_list)
+    return df_metadata
 
 
 def api_get_asset_price(asset_tickers: list, conf: dict) -> pd.DataFrame:
@@ -148,6 +192,18 @@ def api_get_currency_rate(currency_list: list) -> pd.DataFrame:
     return df_currencies
 
 
+def asset_names_to_gbq(names: pd.DataFrame, conf: dict) -> None:
+
+    table = conf["dataset"] + "." + "raw_asset_metadatas"
+    pandas_gbq.to_gbq(
+        names,
+        table,
+        project_id=conf["project"],
+        if_exists="replace",
+        credentials=conf["credentials"],
+    )
+
+
 def asset_prices_to_gbq(prices: pd.DataFrame, conf: dict) -> None:
 
     table = conf["dataset"] + "." + "raw_asset_prices"
@@ -177,9 +233,12 @@ def main() -> None:
     conf = get_conf()
     googlesheet_to_gbq(conf)
     tickers = gbq_get_tickers(conf)
+    all_tickers = gbq_get_all_tickers(conf)
     currencies = gbq_get_currencies(conf)
+    asset_metadatas = api_get_asset_metadata(all_tickers, conf)
     asset_prices = api_get_asset_price(tickers, conf)
     currency_rates = api_get_currency_rate(currencies)
+    asset_names_to_gbq(asset_metadatas, conf)
     asset_prices_to_gbq(asset_prices, conf)
     currency_rates_to_gbq(currency_rates, conf)
 
